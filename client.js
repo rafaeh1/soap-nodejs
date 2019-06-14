b2977c596aeee4a4e8c3926b954d7f5bc39cf195
Rafael Hernández Larios <rafah.larios@gmail.com>
2019-06-14 10:59:21 -0500

    htng testing

--- client.js
+++ client.js
@@ -1,14 +1,81 @@
 'use strict';
 
 var soap = require('soap');
-var url = 'http://127.0.0.1:8000/stockquote?wsdl';
-var endpoint_url = 'http://127.0.0.1:8000/stockquote';
+// var soap = require('strong-soap').soap;
+// var url = 'http://127.0.0.1:8000/stockquote?wsdl';
+// var endpoint_url = 'http://127.0.0.1:8000/stockquote';
 var args = {
   tickerSymbol: 'AAPL'
 };
 
+// var args = {
+//   'tns:tickerSymbol': 'AAPL'
+// };
+
+// soap.createClient(url, (err, client) => {
+//   client.GetLastTradePrice(args, (err, result) => {
+//     console.log(result);
+//   });
+//   client.CheckedIn(args, (err, result) => {
+//     console.log(result);
+//   });
+// }, endpoint_url);
+
+
+var url = 'http://127.0.0.1:8000/GRSM_ServiceExample?wsdl';
+var endpoint_url = 'http://127.0.0.1:8000/GRSM_ServiceExample';
 soap.createClient(url, function(err, client) {
-  client.GetLastTradePrice(args, function(err, result) {
-    console.log(result);
-  });
+  console.log('err: ', err);
+  console.log('client: ', client);
+  if(client) console.log('describe: ', client.describe());
+  // client.GetLastTradePrice(args, (err, result) => {
+  //   console.log(result);
+  // });
+  // client.CheckedIn(args, (err, result) => {
+  //   console.log(result);
+  // });
 }, endpoint_url);
+
+
+//strong
+var options = {};
+// soap.createClient(url, options, function (err, client) {
+//   console.log('err: ',err);
+//   console.log('client: ', client);
+//   // console.log('client.describe(): ', client.describe());
+//   // client.addSoapHeader(`<wsa:Action xmlns:wsa="http://www.w3.org/2005/08/addressing">http://tempuri.org/IWCFAccess/GetOtherData</wsa:Action>`);
+//   // client.addSoapHeader(`<wsa:To xmlns:wsa="http://www.w3.org/2005/08/addressing">https://test-idv.dataventures.com/CP3_WCF_DATA/WCFAccess.svc</wsa:To>`);
+//   // client.CheckedIn(args, function (err, result, envelope, soapHeader) {
+//   //   console.log('result', result);
+//   //   console.log('envelope', envelope);
+//   //   if (err) {
+//   //     throw err;
+//   //   }
+//   //   console.log(result);
+//   // })
+// });
+
+
+
+// soap.createClient(url, function (err, client) {
+//   if (err) {
+//     console.log('Error creating client: ', err);
+//   } else {
+//     console.log('Client created..');
+//     client.Add(args, function (err, result) {
+//       console.log('result ', result);
+//     });
+//   }
+// });
+
+
+
+//strong soap servidor -> OK
+//strong soap client -> describe empty
+
+//strong soap servidor -> OK
+//soap client -> Text data outside of root node.
+
+
+//soap server -> path issue
+
--- index.js
+++ index.js
@@ -1,28 +1,91 @@
 'use strict';
 
 var http = require('http');
-var soap = require('soap');
+// var soap = require('soap');
+const path = require('path');
 var server;
 
-var myService = {
-  StockQuoteService: {
-    StockQuotePort: {
-      GetLastTradePrice: function(args) {
+var soap = require('strong-soap').soap;
+
+// var myService = {
+//   StockQuoteService: {
+//     StockQuotePort: {
+//       GetLastTradePrice: (args) => {
+//         if (args.tickerSymbol === 'trigger error') {
+//           throw new Error('triggered server error');
+//         } else {
+//           return { price: 19.56 };
+//         }
+//       }
+//     }
+//   }
+// };
+
+// HTNGService
+var services = {
+  GRSM_Example: {
+    RoomStayEventConsumer: {
+      CheckedIn: (args) => {
         if (args.tickerSymbol === 'trigger error') {
           throw new Error('triggered server error');
         } else {
-          return { price: 19.56 };
+          return {
+            price: 19.56
+          };
         }
       }
     }
   }
 };
 
-var xml = require('fs').readFileSync('stockquote.wsdl', 'utf8');
+// var services = {
+//   GRSM_Example: {
+//     RoomStayEventConsumer: {
+//       CheckedIn: (args, callback, headers, req) => {
+//         /*
+//           args = {
+//           }
+//         */
+//         //
+//         // Your code
+//         //
+//         /* return {
 
-server = http.createServer(function(request,response) {
-  response.end('404: Not Found: ' + request.url);
+//         }; */
+//         return 'ok';
+//       }
+//     }
+//   }
+// };
+
+// var xml = require('fs').readFileSync('stockquote.wsdl', 'utf8');
+// var xml = require('fs').readFileSync('HTNGService.wsdl', 'utf8');
+
+// console.log('starting...');
+
+// server = http.createServer(function(request,response) {
+//   response.end('404: Not Found: ' + request.url);
+// });
+
+// server.listen(8000);
+// soap.listen(server, '/stockquote', myService, xml);
+// // soap.listen(server, `/GRSM_ServiceExample`, myService, xml);
+// soap.listen(server, path.join(__dirname, 'GRSM_ServiceExample'), services, xml);
+
+
+
+//strong
+var xml = require('fs').readFileSync('HTNGService.wsdl', 'utf8'),
+  server = http.createServer((request, response) => {
+    response.end("404: Not Found: " + request.url);
 });
 
+// console.log('starting...');
+
 server.listen(8000);
-soap.listen(server, '/stockquote', myService, xml);
+soap.listen(server, '/GRSM_ServiceExample', services, xml);
+console.log('listening');
+
+server.log = function (type, data) {
+  console.log('data: ', data);
+};
